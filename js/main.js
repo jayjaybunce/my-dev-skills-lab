5ad26661bda70cf809ed366f0a9d040d9fa925ad
Razu <razu@Razus-MacBook-Pro.local>
2020-02-06 09:13:31 -0700

    fixes issues with button alignment

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+var $buttonEl = $('#addButton')
+// $('.deleteButton').on('click',function(){
+        
+// })
+var $ul = $('ul')
+$buttonEl.on('click',function(){
+    var $inputEl = $('#taskToAdd').val()
+    var $newSkill = $(`
+    <br><button class="deleteButton">X</button><li id="item">${$inputEl}</li>
+    `)
+    $ul.append($newSkill)
+    $('.deleteButton').on('click',function(){
+        $(this).next('li').fadeOut(500,function(){
+            $(this.remove())
+        })
+        $(this).fadeOut(500,function(){
+            $(this.remove())
+        })
+    
+    })
+})
+
+$('.deleteButton').on('click',function(){
+    $(this).next('li').fadeOut(500,function(){
+        $(this.remove())
+    })
+    $(this).fadeOut(500,function(){
+        $(this.remove())
+    })
+
+})
+    
